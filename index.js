74dda615badcb00a49823e28042d37daa1d677f5
mjdev <jervinvillones6@gmail.com>
2026-03-07 16:27:01 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// const img = ["image/img2.png", "image/img4.png", "image/img5.png", "image/img6.png"]
+// const mycolors = ["#1500ff11", "#0000000f", "#ff000015", "#c4ffb739"]
+// const textcolor = ["#1500ff", "#00000076", "#ff0000", "#66D522"]
+// const svgcolors = ["#1500ff", "#00000076", "#ff0000", "#66D522"]
+// let currentimg = 0
+
+// const myimg = document.getElementById("myimage")
+// const textcolors = document.getElementById("colors")
+// const allContents = document.querySelectorAll('[id^="animate"]');
+
+
+// function myimgchange() {
+//     currentimg++
+
+//     if (currentimg >= img.length) {
+//         currentimg = 0
+//     }
+
+//     textcolors.style.color = textcolor[currentimg];
+
+//     allContents.forEach(element => {
+//         element.style.backgroundColor = mycolors[currentimg];
+//     });
+
+//     myimg.src = img[currentimg]
+// }
+
+// setInterval(myimgchange, 4000)
